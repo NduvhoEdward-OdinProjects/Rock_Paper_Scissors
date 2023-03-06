f58ba171c2ac3b019a1ce342c401c0ef8a0ce7b3
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-06 23:56:02 +0200

    Add the getComputeChoice function

---
+++ js/rock_paper_scissors.js
@@ -0,0 +1,11 @@
+console.log("Hellow");
+//let userChoice = prompt("Please enter the name of your move: ");
+//console.log(userChoice); 
+
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choiceIndex = 0;
+    return choices[choiceIndex];
+}
+console.log(getComputerChoice());
+
